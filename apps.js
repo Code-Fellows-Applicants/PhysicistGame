7738a279f7ed97e6340921f9b51f986ba4dbba96
Yonatan1P <yonimotek@gmail.com>
2020-09-18 15:39:10 -0700

    Closes:#6

--- apps.js
+++ apps.js
@@ -1,13 +1,14 @@
 'use strict'
-
+//an empty array which will contain the physicist objects
 var physicistArray=[];
-
-function CreatePhysicist (name, img){
+//a constructor function which creates all of the physicist objects
+    //each object will contain a name and imgPath property
+function CreatePhysicist (name, imgPath){
     this.name = name;
-    this.img = img;
+    this.imgPath = imgPath;
     physicistArray.push(this);
 };
-
+//these are all the physicist being created by instantiating the CreatePhysicist function
 var albertEinstein = new CreatePhysicist ('Albert Einstein', 'images/albertEinstein');
 var marieCurie = new CreatePhysicist('Marie Curie', 'images/marieCurie.jpg');
 var maxPlanck = new CreatePhysicist ('Max Planck', 'images/maxPlanck.jpg');
@@ -21,3 +22,18 @@ var paulDirac = new CreatePhysicist ('Paul Dirac', 'images/paulDirac.jpg');
 var jamesMaxwell = new CreatePhysicist('James Maxwell', 'images/jamesMaxwell.jpg');
 var juliusOppenheimer = new CreatePhysicist('Julius Oppenheimer', 'images/juliusOppenheimer');
 
+//this function will display the current physicists image on the div for the image
+    //it creates an image element and appends the previous div labelled with the id "displayImage"
+function showPhysicist(){
+    for (let i = 0; i < physicistArray.length ; i++ ) {
+        if (counter === i) {
+            var thePhysicist = document.getElementById('displayImage');
+            var physicistPic = document.createElement('img');
+            physicistPic.id = physicistArray[i].name;
+            physicistPic.src = physicistArray[i].imgPath;
+            thePhysicist.appendChild(physicistPic);
+        }
+    }
+}
+//this is the first instance of displaying the physicist once the user loads into the page
+showPhysicist();
